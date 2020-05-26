c75f5b0f820bc11640f89b89d220f7b7dec4d93b
Eugene Suprun <suprune20@gmail.com>
2020-05-26 19:22:58 +0300

    IncognitoUser table (2)

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,12 +6,12 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^api/auth/signup?$', views.api_auth_signup),
-    url(r'^api/auth/signin?$', views.api_auth_signup,  {'signin': True}),
+    url(r'^api/auth/signup/?$', views.api_auth_signup),
+    url(r'^api/auth/signin/?$', views.api_auth_signup,  {'signin': True}),
 
-    url(r'^api/auth/signup/incognito?$', views.api_auth_signup_incognito),
+    url(r'^api/auth/signup/incognito/?$', views.api_auth_signup_incognito),
 
-    url(r'^api/auth/dummy?$', views.api_auth_dummy),
+    url(r'^api/auth/dummy/?$', views.api_auth_dummy),
 
     url(r'^api/download\-apk\-details/?$', views.api_download_apk_details),
     url(r'^api/getlatestversion/?$', views.api_latest_version),
