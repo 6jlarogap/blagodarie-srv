fac31246272824e9d6519b9a5d023ec0a364a37d
Eugene Suprun <suprune20@gmail.com>
2021-02-23 16:33:50 +0300

    security adds

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '25!&dib$-$k*&77*aoy*582#vzqz6q%iuz#pfrhl*zwz0-vq56'
+# Перенесён в local_settings.py
+#
+SECRET_KEY = 'secret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
