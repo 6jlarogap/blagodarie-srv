e512f175dfcd7fcfc93a80000817369a65459366
Eugene Suprun <suprune20@gmail.com>
2023-03-29 18:00:04 +0300

    choose thumbnail method refactor

--- app/app/models.py
+++ app/app/models.py
@@ -712,6 +712,20 @@ class PhotoModel(FilesMixin, models.Model):
         """
         return PhotoModel.choose_photo_of(request, self.photo and self.photo.name or '')
 
+    @classmethod
+    def image_thumb(cls, request, fname,
+            width=THUMB_WIDTH, height=THUMB_HEIGHT,
+            method=THUMB_METHOD
+        ):
+        path = '%(path_to_media)s%(fname)s/%(width)sx%(height)s~%(method)s~12.jpg'  % dict(
+                path_to_media=settings.THUMBNAILS_STORAGE_BASE_PATH,
+                fname=fname,
+                width=width,
+                height=height,
+                method=method,
+        )
+        return request.build_absolute_uri(path)
+
     def choose_thumb(self, request,
         width=THUMB_WIDTH, height=THUMB_HEIGHT,
         method=THUMB_METHOD,
@@ -723,14 +737,9 @@ class PhotoModel(FilesMixin, models.Model):
         if not fname and put_default_avatar:
             fname = default_avatar_in_media
         if fname:
-            path = '%(path_to_media)s%(fname)s/%(width)sx%(height)s~%(method)s~12.jpg'  % dict(
-                    path_to_media=settings.THUMBNAILS_STORAGE_BASE_PATH,
-                    fname=fname,
-                    width=width,
-                    height=height,
-                    method=method,
+            result = PhotoModel.image_thumb(request, fname,
+                width=width, height=height, method=method
             )
-            result = request.build_absolute_uri(path)
         return result
 
 
