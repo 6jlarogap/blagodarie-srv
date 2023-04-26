e4c484c02b00dcd8d22bba88f774d42c50cf210c
Eugene Suprun <suprune20@gmail.com>
2023-04-26 18:25:25 +0300

    Cache settings to local settings, to facilitate update to django 4

--- app/app/settings.py
+++ app/app/settings.py
@@ -465,16 +465,6 @@ LOGGING = {
     }
 }
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
-
 try:
     from app.local_settings import *
 except ModuleNotFoundError:
